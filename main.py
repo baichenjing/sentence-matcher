b3da8e5c1309502ffc00509337147c64dd4c2908
WzyJerry <wzy950507@163.com>
2018-12-10 00:55:11 +0800

    segment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import codecs
 import argparse
 
+from matcher.matcher import Matcher
 from numpy import random
 
 from utils.hierarchy import hierarchy, link_entity, str_stat
@@ -10,6 +11,7 @@ random.seed(0)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', required=True, help='input file')
+parser.add_argument('-q', '--query', required=True, help='query file')
 
 args = parser.parse_args()
 
@@ -17,4 +19,8 @@ with codecs.open(args.file, 'r', encoding='utf-8') as fin:
   result = hierarchy(json.load(fin))
   if result[0]:
     root = result[1]
-    print(root)
+    with codecs.open(args.query, 'r', encoding='utf-8') as qin:
+      query = json.load(qin)
+      matcher = Matcher(root, query['entities'])
+      matcher.match(query['query'])
+      matcher.print_match(matcher.root)
